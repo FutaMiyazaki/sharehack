b07aef1f770f923146e6e9c1585b33f9f1616156
capcap-jp <miyamiyacap@gmail.com>
2021-06-02 01:20:43 +0900

    add:いいね機能に対するrspecの追加 #15

--- back/spec/models/item_like_spec.rb
+++ back/spec/models/item_like_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ItemLike, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { user.item_likes.build(params) }
+  let(:user) { create(:user) }
+  let(:item) { create(:item) }
+  let(:params) { { user_id: user.id, item_id: item.id } }
+
+  context '全てのパラメータが正しい場合' do
+    it { is_expected.to be_valid }
+  end
 end
--- back/spec/requests/api/v1/item_likes_spec.rb
+++ back/spec/requests/api/v1/item_likes_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::ItemLikes", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/api/v1/item_likes/create"
-      expect(response).to have_http_status(:success)
+RSpec.describe 'Api::V1::ItemLikes', type: :request do
+  let(:user) { create(:user) }
+  let(:item) { create(:item) }
+  describe 'POST /api/v1/item_likes' do
+    it 'アイテムにいいね付与ができる' do
+      post api_v1_item_likes_path, params: { user_id: user.id , item_id: item.id }
+      expect(response).to have_http_status 200
+    end
+  end
+
+  describe 'DELETE /api/v1/item_likes' do
+    it 'アイテムに付与済みいいねの取り消しができる' do
+      post api_v1_item_likes_path, params: { user_id: user.id , item_id: item.id }
+      delete api_v1_item_likes_path, params: { user_id: user.id , item_id: item.id }
+      expect(response).to have_http_status 200
     end
   end
 
