cfebd1de798412e83b29f58a0ea8c63f9bf8151c
capcap-jp <miyamiyacap@gmail.com>
2021-07-05 20:29:11 +0900

    add:vuexに関するテストを追加 #39

---
+++ front/test/store/flashMessage.spec.js
@@ -0,0 +1,23 @@
+const Vuex = require('vuex')
+const messages = require('../../store/flashMessage')
+const { createLocalVue } = require('@vue/test-utils')
+const cloneDeep = require('lodash.clonedeep')
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('フラッシュメッセージ', () => {
+  let store
+
+  beforeEach(() => {
+    store = new Vuex.Store(cloneDeep(messages))
+  })
+
+  describe('actions', () => {
+    test('フラッシュメッセージが表示されるか', async () => {
+      const payload = { text: "ログインに成功しました", type: "success", status: "true" }
+      await store.dispatch('showMessage', payload)
+      expect(store.getters['text']).toEqual(payload.text)
+    })
+  })
+})
