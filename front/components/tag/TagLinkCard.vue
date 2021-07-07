c546faa8530652942bd92bffe123f3bfe93dd185
capcap-jp <miyamiyacap@gmail.com>
2021-07-07 23:21:52 +0900

    add:タグ投稿一覧ページへの誘導リンクの作成 #45

---
+++ front/components/tag/TagLinkCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card flat>
+    <v-row>
+      <v-col cols="6"></v-col>
+      <v-col cols="6">{{ tagName }}</v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {}
+</script>
