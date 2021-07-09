f49f4cd5619ceb48ccc8dedef15990483bd4882f
capcap-jp <miyamiyacap@gmail.com>
2021-07-09 20:34:00 +0900

    add:ローディング表示の追加 #45

--- front/components/user/UserItems.vue
+++ front/components/user/UserItems.vue
@@ -1,54 +1,65 @@
 <template>
   <v-row>
-    <v-col v-for="item in items" :key="item.id" cols="12" sm="6">
-      <v-hover v-slot="{ hover }">
-        <v-card
-          outlined
-          :elevation="hover ? 16 : 0"
-          :class="{ 'on-hover': hover }"
-        >
-          <nuxt-link :to="'/item/' + item.id">
-            <v-img aspect-ratio="1" :src="item.image_url" />
-          </nuxt-link>
-          <v-card-title class="py-0">{{ item.name }}</v-card-title>
-          <v-card-actions>
-            <v-row class="px-1 py-2">
-              <v-chip
-                v-for="tag in item.tags"
-                :key="`item-${item.id}-tag-${tag.id}`"
-                label
-                outlined
-                small
-                class="ma-1"
-                @click="toTagItems(tag.id)"
-              >
-                <v-icon small class="mr-1">mdi-tag</v-icon>{{ tag.name }}
-              </v-chip>
-            </v-row>
-          </v-card-actions>
-        </v-card>
-      </v-hover>
-    </v-col>
-    <v-col v-if="!items.length" class="white">
-      <p class="text-center">まだ投稿はありません</p>
-    </v-col>
+    <Loading v-show="loadShow" />
+    <template v-if="!loadShow">
+      <v-col v-for="item in items" :key="item.id" cols="12" sm="6">
+        <v-hover v-slot="{ hover }">
+          <v-card
+            outlined
+            :elevation="hover ? 16 : 0"
+            :class="{ 'on-hover': hover }"
+          >
+            <nuxt-link :to="'/item/' + item.id">
+              <v-img aspect-ratio="1" :src="item.image_url" />
+            </nuxt-link>
+            <v-card-title class="py-0">{{ item.name }}</v-card-title>
+            <v-card-actions>
+              <v-row class="px-1 py-2">
+                <v-chip
+                  v-for="tag in item.tags"
+                  :key="`item-${item.id}-tag-${tag.id}`"
+                  label
+                  outlined
+                  small
+                  class="ma-1"
+                  @click="toTagItems(tag.id)"
+                >
+                  <v-icon small class="mr-1">mdi-tag</v-icon>{{ tag.name }}
+                </v-chip>
+              </v-row>
+            </v-card-actions>
+          </v-card>
+        </v-hover>
+      </v-col>
+      <v-col v-if="!items.length" class="white">
+        <p class="text-center">まだ投稿はありません</p>
+      </v-col>
+    </template>
   </v-row>
 </template>
 
 <script>
+import Loading from '~/components/layout/Loading.vue'
+
 export default {
+  components: {
+    Loading
+  },
   data() {
     return {
-      items: []
+      items: [],
+      loadShow: true
     }
   },
   created() {
     this.$axios
       .get(`api/v1/users/${this.$route.params.id}`)
       .then((response) => {
+        this.loadShow = false
         this.items = response.data.items
       })
       .catch((error) => {
+        this.loadShow = false
         return error
       })
   },
--- front/components/user/UserLikes.vue
+++ front/components/user/UserLikes.vue
@@ -1,32 +1,41 @@
 <template>
   <v-row>
-    <v-col v-for="itemLike in itemLikes" :key="itemLike.id" cols="12" sm="6">
-      <ItemCard :item="itemLike.item" />
-    </v-col>
-    <v-col v-if="!itemLikes.length" class="white">
-      <p class="text-center ma-0">まだ投稿はありません</p>
-    </v-col>
+    <Loading v-show="loadShow" />
+    <template v-if="!loadShow">
+      <v-col v-for="itemLike in itemLikes" :key="itemLike.id" cols="12" sm="6">
+        <ItemCard :item="itemLike.item" />
+      </v-col>
+      <v-col v-if="!itemLikes.length" class="white">
+        <p class="text-center ma-0">まだ投稿はありません</p>
+      </v-col>
+    </template>
   </v-row>
 </template>
 
 <script>
+import Loading from '~/components/layout/Loading.vue'
 import ItemCard from '~/components/item/ItemCard.vue'
+
 export default {
   components: {
+    Loading,
     ItemCard
   },
   data() {
     return {
-      itemLikes: []
+      itemLikes: [],
+      loadShow: true
     }
   },
   created() {
     this.$axios
       .get(`api/v1/users/${this.$route.params.id}/like`)
       .then((response) => {
+        this.loadShow = false
         this.itemLikes = response.data.item_likes
       })
       .catch((error) => {
+        this.loadShow = false
         return error
       })
   }
