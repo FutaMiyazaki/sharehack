65432b3c65ae2c10c7b4f09ef9bbca9713e92555
capcap-jp <miyamiyacap@gmail.com>
2021-07-15 02:39:58 +0900

    fix:middleware内の記述が間違っていたため修正 #54

--- front/middleware/authenticated.js
+++ front/middleware/authenticated.js
@@ -1,19 +1,9 @@
-// 未ログインユーザーはログインページへリダイレクト
 export default function({ store, route, redirect }) {
   if (
     !store.getters['authentication/isLoggedIn'] &&
-    route.fullPath !== '/' &&
-    route.fullPath !== '/users/login' &&
-    route.fullPath !== '/users/signup' &&
-    route.fullPath !== '/tag/search' &&
-    route.fullPath !== `/users/${route.params.id}` &&
-    route.fullPath !== `/mylike/${route.params.id}` &&
-    route.fullPath !== `/item/${route.params.id}` &&
-    route.fullPath !== `/item/ranking?page=${route.query.page}` &&
-    route.fullPath !== `/item/latest?page=${route.query.page}` &&
-    route.fullPath !==
-      `/item/search?keyword=${route.query.keyword}&page=${route.query.page}` &&
-    route.fullPath !== `/tag/${route.params.id}?page=${route.query.page}`
+    (route.fullPath === '/item/create' ||
+      route.fullPath === '/users/setting' ||
+      route.fullPath === `/item/edit/${route.params.id}`)
   ) {
     return redirect('/users/login')
   }
