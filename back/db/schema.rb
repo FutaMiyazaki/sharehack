064d51e44f61f08cc5af64d08bb49f799f75640a
capcap-jp <miyamiyacap@gmail.com>
2021-07-21 14:14:52 +0900

    add:Topicテーブルの作成 #58

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_053218) do
+ActiveRecord::Schema.define(version: 2021_07_21_051138) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -89,6 +89,15 @@ ActiveRecord::Schema.define(version: 2021_07_12_053218) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "topics", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_topics_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
@@ -125,4 +134,5 @@ ActiveRecord::Schema.define(version: 2021_07_12_053218) do
   add_foreign_key "items", "users"
   add_foreign_key "relationships", "users"
   add_foreign_key "relationships", "users", column: "follow_id"
+  add_foreign_key "topics", "users"
 end
