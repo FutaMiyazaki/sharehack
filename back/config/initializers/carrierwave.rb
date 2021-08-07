726075cb12371ee43a3c7ad9a263212d0f51e145
capcap-jp <miyamiyacap@gmail.com>
2021-08-07 23:52:46 +0900

    delete:不要な記述を削除 #68

--- back/config/initializers/carrierwave.rb
+++ back/config/initializers/carrierwave.rb
@@ -3,23 +3,12 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.cache_storage = :fog
-    config.fog_directory = 'sharehack-bucket'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'
-    }
-  else
-    config.cache_storage = :fog
-    config.fog_directory = 'sharehack-bucket'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: 'AKIAYVGY7HKWZSL4FCJ4',
-      aws_secret_access_key: '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',
-      region: 'ap-northeast-1'
-    }
-  end
+  config.cache_storage = :fog
+  config.fog_directory = 'sharehack-bucket'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: 'ap-northeast-1'
+  }
 end
